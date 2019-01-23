d76f7588686c1f5ce80a1a84c8361ea99c6299cc
Yashvardhan Singh <yashvardhan17123@iiitd.ac.in>
2019-01-24 01:35:16 +0530

    Implemented parsing and data storage.

--- src/Assignment1/Main.java
+++ src/Assignment1/Main.java
@@ -2,11 +2,14 @@ package Assignment1;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
 
 class Field{
     private String name;
     private String type;
     private String size;
+    private int id;
     Field(String name,String type,String size){
         this.name=name;
         this.type=type;
@@ -24,7 +27,19 @@ class Field{
     public String getSize() {
         return size;
     }
+
+    public int getId(){
+        return id;
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+
+
 }
+
+
 
 public class Main {
 
@@ -39,11 +54,44 @@ public class Main {
         ArrayList<Field> structure=new ArrayList<Field>();
 
         String r=br2.readLine();
+        int inticount = -1;
+        int fcount = -1;
+        int charcount = -1;
+
+
         while(r!=null){
             String[] a=r.split(",");
             Field f=new Field(a[0],a[1],a[2]);
+            if(a[1].equals("I")){
+                inticount++;
+                f.setId(inticount);
+            }
+            else if(a[1].equals("F")){
+                fcount++;
+                f.setId(fcount);
+            }
+            else if(a[1].equals("C")){
+                charcount++;
+                f.setId(charcount);
+            }
             structure.add(f);
             r=br2.readLine();
+        }
+        System.out.println(inticount+" "+charcount+" "+fcount);
+        ArrayList intis[] = new ArrayList[inticount+1];
+        ArrayList floats[] = new ArrayList[fcount+1];
+        ArrayList chars[] = new ArrayList[charcount+1];
+
+        for(int i = 0;i<inticount+1;i++){
+            intis[i] = new ArrayList<Integer>();
+        }
+
+        for(int i = 0;i<fcount+1;i++){
+            floats[i] = new ArrayList<Float>();
+        }
+
+        for(int i = 0;i<charcount+1;i++){
+            chars[i] = new ArrayList<String>();
         }
 
         for(int i=0;i<structure.size();i++){
@@ -64,9 +112,116 @@ public class Main {
             index++;
         }
         br1.close();
-        for(int i=0;i<3;i++){
-            System.out.println(records[i]);
+
+
+
+        Scanner sc = new Scanner(data_file);
+
+        while(sc.hasNext()){
+            String str = sc.nextLine();
+            int start = 0;
+            for(int j = 0;j<structure.size();j++){
+                Field field = structure.get(j);
+                String type = field.getType();
+
+                int bound = Integer.parseInt(field.getSize());
+                int id = field.getId();
+
+                String req = "";
+
+                if(type.equals("I")){
+                    for(int f = start;f < start+bound;f++){
+                        if(f >= str.length()){
+                            try{
+                                int parsed = Integer.parseInt(req);
+                                System.out.println(parsed+"p");
+                                //System.out.println(id+"id");
+                                intis[id].add(parsed);
+                            }
+                            catch (Exception e){
+                                System.out.println(e);
+                            }
+                            break;
+                        }
+                        if(str.substring(f,f+1).equals(" ")){
+                            try{
+                                int parsed = Integer.parseInt(req);
+                                //System.out.println(parsed+"p");
+                                //System.out.println(id+"id");
+                                intis[id].add(parsed);
+                            }
+                            catch (Exception e){
+                                System.out.println(e);
+                            }
+                            break;
+                        }
+                        else{
+                            req += str.substring(f,f+1);
+                        }
+                    }
+                }
+                else if(type.equals("F")){
+                    for(int f = start;f < start+bound;f++){
+                        if(f >= str.length()) {
+                            try{
+                                float parsed = Float.parseFloat(req);
+                                floats[id].add(parsed);
+                            }
+                            catch (Exception e){
+                                System.out.println(e);
+                            }
+                            break;
+                        }
+                        if(str.substring(f,f+1).equals(" ")){
+                            try{
+                                float parsed = Float.parseFloat(req);
+                                floats[id].add(parsed);
+                            }
+                            catch (Exception e){
+                                System.out.println(e);
+                            }
+                            break;
+                        }
+                        else{
+                            req += str.substring(f,f+1);
+                        }
+                    }
+                }
+                else if(type.equals("C")){
+                    for(int f = start;f < start+bound;f++){
+                        if(f >= str.length()){
+                            chars[id].add(req);
+                            break;
+                        }
+                        if(str.substring(f,f+1).equals(" ")){
+                            chars[id].add(req);
+                            break;
+                        }
+                        else{
+                            req += str.substring(f,f+1);
+                        }
+                    }
+                }
+                start += bound;
+                //System.out.println(req);
+            }
+
         }
+
+        for(int i = 0;i<inticount+1;i++){
+            Collections.sort(intis[i]);
+        }
+
+        for(int i = 0;i<fcount+1;i++){
+            Collections.sort(floats[i]);
+        }
+
+        for(int i = 0;i<charcount+1;i++){
+            Collections.sort(chars[i]);
+        }
+        
+
+
 
     }
 }
