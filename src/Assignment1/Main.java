7dd03bf26344f5deb77b079b4a9053218bdd062f
anmolsinghal <anmolsinghal1998@gmail.com>
2019-01-24 15:35:14 +0530

    max value code added

--- src/Assignment1/Main.java
+++ src/Assignment1/Main.java
@@ -51,13 +51,13 @@ public class Main {
         BufferedReader br1=new BufferedReader(new FileReader(data_file));
         BufferedReader br2=new BufferedReader(new FileReader(meta_file));
 
-        ArrayList<Field> structure=new ArrayList<Field>();
+        ArrayList<Field> structure=new ArrayList<>();
 
         String r=br2.readLine();
         int inticount = -1;
         int fcount = -1;
         int charcount = -1;
-
+        int doublecount=-1;
 
         while(r!=null){
             String[] a=r.split(",");
@@ -74,13 +74,17 @@ public class Main {
                 charcount++;
                 f.setId(charcount);
             }
+            else if(a[1].equals("D")){
+                doublecount++;
+                f.setId(doublecount);
+            }
             structure.add(f);
             r=br2.readLine();
         }
-        System.out.println(inticount+" "+charcount+" "+fcount);
         ArrayList intis[] = new ArrayList[inticount+1];
         ArrayList floats[] = new ArrayList[fcount+1];
         ArrayList chars[] = new ArrayList[charcount+1];
+        ArrayList doubles[] = new ArrayList[doublecount+1];
 
         for(int i = 0;i<inticount+1;i++){
             intis[i] = new ArrayList<Integer>();
@@ -94,6 +98,10 @@ public class Main {
             chars[i] = new ArrayList<String>();
         }
 
+        for(int i=0;i<doublecount+1;i++){
+            doubles[i]=new ArrayList<Double>();
+        }
+
         for(int i=0;i<structure.size();i++){
             System.out.println("Field Name: "+structure.get(i).getName()+", Field Type: "+structure.get(i).getType()+", Field Size: "+structure.get(i).getSize());
         }
@@ -102,23 +110,11 @@ public class Main {
         System.out.println("Finish reading data description file...");
         System.out.println();
 
-        String[] records=new String[100];
-        int index=0;
-
-        r=br1.readLine();
-        while(r!=null){
-            records[index]=r;
-            r=br1.readLine();
-            index++;
-        }
-        br1.close();
-
-
-
         Scanner sc = new Scanner(data_file);
 
         while(sc.hasNext()){
             String str = sc.nextLine();
+            System.out.println(str);
             int start = 0;
             for(int j = 0;j<structure.size();j++){
                 Field field = structure.get(j);
@@ -134,8 +130,6 @@ public class Main {
                         if(f >= str.length()){
                             try{
                                 int parsed = Integer.parseInt(req);
-                                System.out.println(parsed+"p");
-                                //System.out.println(id+"id");
                                 intis[id].add(parsed);
                             }
                             catch (Exception e){
@@ -146,8 +140,6 @@ public class Main {
                         if(str.substring(f,f+1).equals(" ")){
                             try{
                                 int parsed = Integer.parseInt(req);
-                                //System.out.println(parsed+"p");
-                                //System.out.println(id+"id");
                                 intis[id].add(parsed);
                             }
                             catch (Exception e){
@@ -202,6 +194,28 @@ public class Main {
                         }
                     }
                 }
+                else if(type.equals("D")){
+                    for(int f=start;f<start+bound;f++){
+                        if(f>=str.length()){
+                            Double d=Double.parseDouble(req);
+                            doubles[id].add(d);
+                            break;
+                        }
+                        if(str.substring(f,f+1).equals(" ")){
+                            try{
+                                Double d=Double.parseDouble(req);
+                                doubles[id].add(d);
+                            }
+                            catch(Exception e){
+                                System.out.println(e);
+                            }
+                            break;
+                        }
+                        else{
+                            req+=str.substring(f,f+1);
+                        }
+                    }
+                }
                 start += bound;
                 //System.out.println(req);
             }
@@ -219,9 +233,32 @@ public class Main {
         for(int i = 0;i<charcount+1;i++){
             Collections.sort(chars[i]);
         }
-        
 
+        for(int i=0;i<doublecount+1;i++){
+            System.out.println(doubles[i]);
+        }
 
+        int flag=0;
+        String max_field=args[0];
+        for(int i=0;i<structure.size();i++){
+            if(max_field.equals(structure.get(i).getName())){
+                flag++;
+                if(structure.get(i).getType().equals("I")){
+                    System.out.println("Maximum value in field" + structure.get(i).getName() + ": " + intis[structure.get(i).getId()].get(intis[structure.get(i).getId()].size()-1));
+                }
+                else if(structure.get(i).getType().equals("C")){
+                    System.out.println("Maximum value in field" + structure.get(i).getName() + ": " + chars[structure.get(i).getId()].get(chars[structure.get(i).getId()].size()-1));
+                }
+                else if(structure.get(i).getType().equals("F")){
+                    System.out.println("Maximum value in field" + structure.get(i).getName() + ": " + floats[structure.get(i).getId()].get(floats[structure.get(i).getId()].size()-1));
+                }
+                else{
+                    if(structure.get(i).getType().equals("D")){
+                        System.out.println("Maximum value in field" + structure.get(i).getName() + ": " + doubles[structure.get(i).getId()].get(doubles[structure.get(i).getId()].size()-1));
+                    }
+                }
+            }
+        }
 
     }
 }
